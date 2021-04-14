2217f7dc8f52a2e4462328493a65d0642e59d9a3
Gustavo Villalta <gvillalta99@users.noreply.github.com>
2021-04-14 10:32:28 -0300

    Update

--- 2021-04-13/function_call_by_reference.c
+++ 2021-04-13/function_call_by_reference.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-	* by_reference - Function call by reference
-	* @n: integer pointer
-	*
-	* Description: Function that access a value
-	*
-	* Return: nothing
-	*/
-int *by_reference(int *n)
+ * by_reference - Function call by reference
+ * @n: integer pointer
+ *
+ * Description: Function that access a value
+ *
+ * Return: nothing
+ */
+int by_reference(int *n)
 {
 	printf("n=%p\n", (void *) n);
 	printf("n address=%p\n", (void *) (&n));
@@ -20,16 +20,16 @@ int *by_reference(int *n)
 	printf("n address=%p\n", (void *) (&n));
 	printf("n value=%d\n", (*n));
 
-	return (n);
+	return 0;
 }
 
 /**
-	* main - Playground for live coding
-	*
-	* Description: Playground function for our live coding session
-	*
-	* Return: Exit status, 0 for success, error otherwise
-	*/
+ * main - Playground for live coding
+ *
+ * Description: Playground function for our live coding session
+ *
+ * Return: Exit status, 0 for success, error otherwise
+ */
 int main(void)
 {
 	int d = 5;
